a67646ee49e4cbfb8d542ca18e5e4daa30add8bd
Oleh Mazur <oleh@four-velocity.com>
2022-09-18 19:13:20 +0200

    :sparkles: Add config class

---
+++ app/config.py
@@ -0,0 +1,36 @@
+__all__ = [
+    "get_settings"
+]
+from functools import lru_cache
+from typing import Any
+
+from pydantic import BaseSettings, validator, PostgresDsn
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_URI: PostgresDsn | None = None
+
+    @validator("POSTGRES_URI", pre=True)
+    def assemble_db_connection(cls, v: str | None, values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return Any
+        return PostgresDsn.build(
+            scheme="postgres",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_HOST"),
+            port=values.get("POSTGRES_PORT"),
+            path=values.get(f"/{values.get('POSTGRES_DB') or ''}")
+        )
+
+    FINNHUB_SECRET: str
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
